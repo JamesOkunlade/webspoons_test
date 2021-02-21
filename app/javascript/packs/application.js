92e70d2d494ad9b580b9831ed84b21f0a78ad3ca
James Okunlade <james.okunlade@gmail.com>
2021-02-21 23:24:56 +0100

    fix webpack compilation error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,9 @@
 
 // import bootstrap
 
+import jquery from 'jquery';
+window.$ = window.jquery = jquery;
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
